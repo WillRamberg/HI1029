e4b7310eab59fcd5d3e9f481cf55bf72abfe36cb
William Ramberg <117517479+WillRamberg@users.noreply.github.com>
2024-02-20 16:18:52 +0100

    Labb3 U7

--- src/main/java/com/example/hi1029/Labb3/Puzzle.java
+++ src/main/java/com/example/hi1029/Labb3/Puzzle.java
@@ -3,15 +3,20 @@ package com.example.hi1029.Labb3;
 public class Puzzle {
 
     public static void main(String[] args) {
-        var puzzle = new Puzzle(1, 3);
+        var puzzle = new Puzzle(3, 3);
         puzzle.solve();
+
+        for(int i = 1; i < 6; i++)
+            for(int j = 1; j < 6; j++){
+                Puzzle puzz = new Puzzle(i, j);
+                puzz.solve();
+            }
     }
 
 
     public class position{
         boolean occupied;
         char symbol;
-
         public position(boolean occupied, char symbol){
             this.occupied = occupied;
             this.symbol = symbol;
@@ -59,8 +64,8 @@ public class Puzzle {
         }
     }
 
-    int blockedY, blockedX;
-    position[][] board;
+    private int blockedY, blockedX;
+    private position[][] board;
     private int nrOfSolutions = 0;
     public Puzzle(int blockedY, int blockedX){
         this.blockedX = blockedX;
@@ -81,30 +86,29 @@ public class Puzzle {
         }
     }
 
-    private void solve(int piecesPlaced, int y, int x){
-        if(x >= 5 || x < 0)
-            return;
-
-        if(y >= 5 || y < 0)
-            return;
+    private void solve(int piecesPlaced, int rowIndex, int columnIndex){
 
         if(piecesPlaced == 8) {
-            System.out.println(this);
+            //System.out.println(this);
+            nrOfSolutions++;
             return;
         }
 
-        for(int column = y; column < 5; column++){
+        if(rowIndex >= 5 || rowIndex < 0)
+            return;
+
+        for(int column = columnIndex; column < 5; column++){
             for(int typeOfRotation = 0; typeOfRotation < 4; typeOfRotation++){
-                var piece = new Piece(x, column, typeOfRotation);
+                var piece = new Piece(column, rowIndex, typeOfRotation);
                 if(canPlace(piece)){
                     place(piece);
-                    solve(piecesPlaced + 1, y, column + 1);
+                    solve(piecesPlaced + 1, rowIndex, column + 1);
                     unPlace(piece);
                 }
             }
         }
-        if(fullRow(y))
-            solve(piecesPlaced, y + 1, 0);
+        if(fullRow(rowIndex))
+            solve(piecesPlaced, rowIndex + 1, 0);
     }
 
     private boolean canPlace(Piece piece){
@@ -121,14 +125,6 @@ public class Puzzle {
         board[piece.y1][piece.x1] = new position(true, piece.symbol);
         board[piece.y2][piece.x2] = new position(true, piece.symbol);
         board[piece.y3][piece.x3] = new position(true, piece.symbol);
-        var builder = new StringBuilder();
-        for(var row: board){
-            for(var position : row){
-                builder.append(" ").append(position.symbol).append(" ");
-            }
-            builder.append("\n");
-        }
-        System.out.println(builder.toString());
     }
 
     private void unPlace(Piece piece){
